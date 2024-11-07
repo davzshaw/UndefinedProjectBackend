2fc7a56c8d86e2cb30b5f9dabc5a58c98b55e696
CrisHzz <124224845+CrisHzz@users.noreply.github.com>
2024-11-06 20:15:43 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RateLimiterMiddleware } from './middleware/rateLimiter.middleware';
+import { SecurityMiddleware } from './middleware/security.middleware';
 import { Logger } from '@nestjs/common';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'], 
   });
-
   app.enableCors({
-    origin: "https://world-ker.vercel.app", // Tu dominio en Vercel
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    origin: ["https://world-ker.vercel.app/"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: false,
   });
+  app.use(morgan('combined'));
+
+  app.use(new RateLimiterMiddleware().use);
+  app.use(new SecurityMiddleware().use);
 
   await app.listen(3000);
   Logger.log('Application is running on: http://localhost:3000');
 }
-
 bootstrap();
