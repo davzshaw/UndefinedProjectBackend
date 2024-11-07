5c463901a9accf3102383fcb91e254b8ba0646a0
CrisHzz <124224845+CrisHzz@users.noreply.github.com>
2024-11-06 19:36:49 -0500

    Update main.ts (Probando para vercel)

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { RateLimiterMiddleware } from './middleware/rateLimiter.middleware';
-import { SecurityMiddleware } from './middleware/security.middleware';
 import { Logger } from '@nestjs/common';
-import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['log', 'error', 'warn', 'debug', 'verbose'], 
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
-  app.enableCors({
-    origin: ["http://localhost:3000"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: false,
-  });
-  app.use(morgan('combined'));
 
-  app.use(new RateLimiterMiddleware().use);
-  app.use(new SecurityMiddleware().use);
+  app.enableCors({
+    origin: ["https://world-ker.vercel.app"], 
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, 
+  });
+
+  app.options('*', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "https://world-ker.vercel.app"); // Permite solo tu dominio en Vercel
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.sendStatus(200); 
+  });
 
   await app.listen(3000);
   Logger.log('Application is running on: http://localhost:3000');
 }
+
 bootstrap();
